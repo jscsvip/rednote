630c80e6d1db6431160f0225bd51f1d48dbfc83c
李龙 <lilong2576@hanweb.com>
2025-04-17 15:34:15 +0800

    feat: 导入music布局与站点图标

---
+++ app/music/layout.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "AudioPlex",
+  description: "Plex Audio!",
+  icons:{
+    icon: "/music/icon.png",
+    shortcut: "/music/icon.png",
+    apple: "/music/icon.png"
+  }
+};
+
+interface ExamplesLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function ExamplesLayout({ children }: ExamplesLayoutProps) {
+  return (
+  
+        <div className="container relative">
+          <section>
+            <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow">
+              {children}
+            </div>
+          </section>
+        </div>
+      
+  );
+}
