5e537cb1acae3fb813645f989f81dbf786cb1e0f
李龙 <lilong2576@hanweb.com>
2025-04-18 16:22:14 +0800

    feat: music添加tabs切换项

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,6 +1,9 @@
 import Menu from '@/components/music/menu'
 import { Sidebar } from '@/components/music/sidebar';
 import { playlists } from "@/data/playlists";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from '@/components/ui/button';
+import { PlusCircledIcon } from '@radix-ui/react-icons';
 
 export default function MusicPage() {
   return (
@@ -17,7 +20,37 @@ export default function MusicPage() {
              
             <div className="col-span-5 lg:col-span-4">
               <div className="px-4 py-6 lg:px-8">
-                CONTENT
+              <Tabs defaultValue="music" className="h-full space-y-6">
+                    <div className="space-between flex items-center">
+                      <TabsList>
+                        <TabsTrigger value="music" className="relative">
+                          Music
+                        </TabsTrigger>
+                        <TabsTrigger value="podcasts">Podcasts</TabsTrigger>
+                        <TabsTrigger value="live" disabled>
+                          Live
+                        </TabsTrigger>
+                      </TabsList>
+                      <div className="ml-auto mr-4">
+                        <Button>
+                          <PlusCircledIcon className="mr-2 h-4 w-4" />
+                          Add music
+                        </Button>
+                      </div>
+                    </div>
+                    <TabsContent
+                      value="music"
+                      className="border-none p-0 outline-none"
+                    >
+                      music
+                    </TabsContent>
+                    <TabsContent 
+                    value="podcasts"
+                    className="h-full flex-col border-none p-0 data-[state=active]:flex"
+                    >
+                      postcasts
+                    </TabsContent>
+                </Tabs>
 
               </div>
             </div>
